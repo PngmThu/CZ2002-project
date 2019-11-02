4748e2decacf05df863080c160465ced9f7d803e
xiaochaorenn <54619857+xiaochaorenn@users.noreply.github.com>
2019-11-02 21:56:19 +0800

    Updated Views
    Included all the different views users will interact with:
    - BookingHistoryView
    -LoginView
    -MainMenuView
    -MovieListingView
    -SearchMovieView

---
+++ Project/src/view/BookingHistoryVIew.java
@@ -0,0 +1,7 @@
+package view;
+
+public class BookingHistoryVIew { //Allow Moviegoers to view booking history
+    
+    //DO NOT have to implement login, as we can simply use their email address and mobile number to go the entity to
+    //grab their booking history. Pass this to BookingHistoryCtrl to get all their past bookings
+}
---
+++ Project/src/view/MovieListingView.java
@@ -0,0 +1,5 @@
+package view;
+
+public class MovieListingView {
+    //Display a list of all the movies available in Moblima to Moviegoers, using MovieListingViewCtrl.
+}
