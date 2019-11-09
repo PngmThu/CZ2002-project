bec567b479ca2137d4e5c68c26580ed896ccbfcf
gsheng19 <litekirax3@gmail.com>
2019-11-09 16:49:01 +0800

    updated MovieDetailsCtrl

--- Project/src/view/MovieDetailView.java
+++ Project/src/view/MovieDetailView.java
@@ -2,13 +2,40 @@ package view;
 
 import Controllers.MovieDetailsCtrl;
 import Controllers.MovieReviewCtrl;
+import Entities.Movie;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MovieDetailView {
+public class MovieDetailView extends MoblimaViews{
+    private String movieTitle;
 
-    public static void main(String[] args){
-
+    public void enterView(){
+        //pass movieTitle
+        MovieDetailsCtrl.displayMovieDetail(movieTitle);
     }
+
+    public static void main(String [] args){
+        int choice;
+        ArrayList<Movie> movies = Movie.getAllMoviesData();
+        Scanner sc = new Scanner(System.in);
+        Movie movie = null;
+        for(int i = 0 ; i < movies.size() ; i++) {
+            movie = movies.get(i);
+        }
+        if(movie.getStatus().equals("Upcoming")){
+
+        }
+
+//        for(i = 0 ; i < movies.size() ; i++) {
+//            Movie movie = movies.get(i);
+//        }
+
+
+        System.out.println("Please select your choice: ");
+        System.out.println("1) Return to Search/List Movies page. ");
+        System.out.println("2) Review/Rate the movie.");
+        choice = sc.nextInt();
+    }
+
 }
